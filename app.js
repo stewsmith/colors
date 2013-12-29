fa6579d0888214b2db6ea5603759ced307df3c0c
Stewart Smith <stewartlsmith12@gmail.com>
2013-12-29 04:51:49 +0000

    socket io bitching

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -32,4 +34,8 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket) {
+    console.log('here')
 });
