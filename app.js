4003008ac3968ddfabe7d5b1259a391b5997ace6
Stewart Smith <stewartlsmith12@gmail.com>
2013-12-30 00:47:04 +0000

    back end archtecture

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var server = http.createServer(app)
 var io = require('socket.io').listen(server);
 
 // all environments
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -29,20 +29,34 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-server.listen(app.get('port'), function(){
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
 var userCount = 0;
+var scoreSum = 0;
+
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 io.sockets.on('connection', function(socket) {
     userCount++;
     socket.on('disconnect', function(sessionID) {
+        console.log('disconnecting');
+        scoreSum -= parseInt(socket["currScore"]);
+        console.log("scoreSum: " + scoreSum);
         userCount--;
-        console.log("userCount: " + userCount)
     });
-    console.log("userCount: " + userCount)
+    socket.on('scoreChange', function(score) {
+        score = parseInt(score, 10);
+        if(score) {
+            console.log("got it: " + score)
+            if(socket["currScore"]){
+                scoreSum -= parseInt(socket["currScore"]);
+            }
+            scoreSum += score;
+            socket["currScore"] = score;
+            console.log("scoreSum: " + scoreSum);
+        }
+    });
 });
