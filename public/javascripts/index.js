dfce752313540b382f21dbc1e7160886cd73650b
Stewart Smith <stewartlsmith12@gmail.com>
2014-02-03 19:10:41 +0000

    change background color g-y-r

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,7 +14,15 @@ function createSession() {
 }
 
 socket.on('average', function(average) {
-    var ending_color = new $.Color( 'rgb(255,0,0)' ); ;//what color we want to use in the end
+    var ending_color;
+    if(average == 0) {
+        ending_color = new $.Color( 'rgb(255,0,0)' );
+    }
+    else if(average < 0.5) {
+        ending_color = new $.Color( 'rgb(255,255,0)' );
+    }
+    else if(average > 0.5) {
+        ending_color = new $.Color( 'rgb(255,0,0)' );
+    }
     $('body').animate({ backgroundColor: ending_color }, 0);
-    console.log("average is: " + average);
 });
