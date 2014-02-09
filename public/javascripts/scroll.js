68fed0a7c3b48916617d35a5de8d1ee8aef2f522
Stewart Smith <stewartlsmith12@gmail.com>
2014-02-09 20:31:38 +0000

    smooth scroll from green to red

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -3,24 +3,30 @@ $(document).ready(function(){
     //** http://code.jquery.com/color/jquery.color-2.1.0.js
 
     var scroll_pos = 0;
-    var animation_begin_pos = 0; //where you want the animation to begin
-    var animation_end_pos = 1000; //where you want the animation to stop
-    var beginning_color = new $.Color( 'rgb(0,255,0)' ); //we can set this here, but it'd probably be better to get it from the CSS
-    var ending_color = new $.Color( 'rgb(255,0,0)' ); ;//what color we want to use in the end
+    var animation_begin_pos = 0;
+    var animation_end_pos = 1000;
+    var beginning_color = new $.Color( 'rgb(0,255,0)' );
+    var mid_color = new $.Color( 'rgb(255,255,0)' );
+    var ending_color = new $.Color( 'rgb(255,0,0)' );
+
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) {
-            //we want to calculate the relevant transitional rgb value
-            var percentScrolled = scroll_pos / ( animation_end_pos - animation_begin_pos );
-            var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
-            var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
-            var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
-            var newColor = new $.Color( newRed, newGreen, newBlue );
+        var percentScrolled = scroll_pos / (animation_end_pos - animation_begin_pos);
+        // Green to yellow
+        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos && percentScrolled < 0.5) {
+            var newRed =  mid_color.red() * percentScrolled * 2;
+            var newColor = new $.Color(newRed, 255, 0);
+            $('body').animate({ backgroundColor: newColor }, 0);
+        }
+        // Yellow to Red
+        else if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos && percentScrolled > 0.5) {
+            var newGreen = mid_color.green() - (mid_color.green() * (percentScrolled - 0.5) * 2);
+            var newColor = new $.Color(255, newGreen, 0);
             $('body').animate({ backgroundColor: newColor }, 0);
         } else if ( scroll_pos > animation_end_pos ) {
-             $('body').animate({ backgroundColor: ending_color }, 0);
+            $('body').animate({ backgroundColor: ending_color }, 0);
         } else if ( scroll_pos < animation_begin_pos ) {
-             $('body').animate({ backgroundColor: beginning_color }, 0);
-        } else { }
+            $('body').animate({ backgroundColor: beginning_color }, 0);
+        }
     });
 });
