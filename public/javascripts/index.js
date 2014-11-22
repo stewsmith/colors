789fdaee9b1efedf86dd4c0ffac4e633a06e4618
Stewart Smith <stewart@optimizely.com>
2014-11-22 14:08:34 -0500

    switch to jquery listeners for createSession

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,29 +1,33 @@
 var socket = io.connect('/');
 
-$('#sessionForm').on('submit', function() {
+$('#sessionForm').on('submit', function(e) {
+  e.preventDefault();
   var sessionId = $('#sessionInput').val();
-  window.location = 'user?sessionID=' + sessionID;
+  window.location.href = 'user?sessionID=' + sessionId;
 });
 
 function createSession() {
-    var sessionID = Math.round(Math.random()*10000).toString();
-    socket.emit('create', sessionID);
-    $('#sessionID').append(sessionID);
-    $('#sessionForm').remove();
-    $('#sessionButton').remove();
 }
 
+$('#createSession').on('click', function(e) {
+  var sessionID = Math.round(Math.random()*10000).toString();
+  socket.emit('create', sessionID);
+  $('#sessionID').append(sessionID);
+  $('#sessionForm').remove();
+  $(this).remove();
+});
+
 socket.on('average', function(average) {
-    console.log("average: " + average);
-    var ending_color;
-    if(average == 0) {
-        ending_color = new $.Color( 'rgb(0,255,0)' );
-    }
-    else if(average < 0.5) {
-        ending_color = new $.Color( 'rgb(255,255,0)' );
-    }
-    else if(average > 0.5) {
-        ending_color = new $.Color( 'rgb(255,0,0)' );
-    }
-    $('body').animate({ backgroundColor: ending_color }, 0);
+  console.log("average: " + average);
+  var ending_color;
+  if(average == 0) {
+    ending_color = new $.Color( 'rgb(0,255,0)' );
+  }
+  else if(average < 0.5) {
+    ending_color = new $.Color( 'rgb(255,255,0)' );
+  }
+  else if(average > 0.5) {
+    ending_color = new $.Color( 'rgb(255,0,0)' );
+  }
+  $('body').animate({ backgroundColor: ending_color }, 0);
 });
