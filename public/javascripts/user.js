6fc9689d1a7f97ad55372b5115ee5aecfaf71929
Stewart Smith <stewart@optimizely.com>
2015-11-25 09:35:57 -0800

    styling for student page

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -4,6 +4,8 @@ var sessionId = location.search.substring(location.search.indexOf('=') + 1)
 socket.emit('userJoin', sessionId);
 $('#sessionID').append(sessionId);
 
+window.scrollTo(0, 550);
+
 $(window).scroll(function() {
   var rgb = $('body').css('background-color');
   var studentId = $('body').attr('id');
